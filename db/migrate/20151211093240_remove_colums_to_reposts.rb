b37b5c15cb8501c7b11dd62cd2ee387898dab8f8
kikisatoshi <keyofcastle@gmail.com>
2015-12-11 22:16:30 +0000

    fourth commit

--- db/migrate/20151211093240_remove_colums_to_reposts.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumsToReposts < ActiveRecord::Migration
-  def change
-    remove_reference :reposts, :repost_user, index: true, foreign_key: true
-    remove_reference :reposts, :reposted, index: true, foreign_key: true
-  end
-end
